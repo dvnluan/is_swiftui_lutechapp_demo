6e55e6eed29638941cc09887be76fc3e6c29cf5b
luan <luan@dluan.io.vn>
2026-02-21 14:47:54 +0700

    Initial Commit

---
+++ lutech/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lutech
+//
+//  Created by Doan Luan on 21/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lutech/lutechApp.swift
@@ -0,0 +1,17 @@
+//
+//  lutechApp.swift
+//  lutech
+//
+//  Created by Doan Luan on 21/2/26.
+//
+
+import SwiftUI
+
+@main
+struct lutechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
